347e39cfff53d24b3c0b61b91a06a6a9bb4f87e9
Ragul1902 <ragulsenthilkumar02@gmail.com>
2021-11-20 00:19:16 +0530

    Added code for malloc

--- malloc.c
+++ malloc.c
@@ -3,13 +3,32 @@
 #include<stdio.h>
 #include"malloc.h"
 
+/*
+ * This implementation of malloc is based on first-fit style.
+ * meta_block is a struct to store the meta information about every chuck of memory being allocated.
+ * A Singly-linked list is maintained with all the meta_blocks 
+ * to maintain the space being allocated and deallocated
+ * Size of meta_block is considered to be 12 (4 bytes for the variables free, size and next). 
+ */
+
 #define META_BLOCK_SIZE 12
+ 
+/* The macro align4 is used to set the requested size to multiple of four greater than requested size */
 #define align4(x) (((((x) -1) > >2) < <2)+4)
 
+/* meta_ptr is a pointer of type meta_block, it is type defined for simplicity and to avoid confusion */
 typedef struct meta_block *meta_ptr;
 
+/* base stores the head of the linked list */
 void* base = NULL;
 
+/*
+ * The meta_block contains the variables free, size, next, data.
+ * free is set to 1 when the respective block is free and vice versa.
+ * size is used to store the size of the respective block.
+ * The character array data gives the next address after the 
+ * meta_block as the data variable is defined at last.
+ */
 struct meta_block
 {
     int free;
@@ -18,6 +37,12 @@ struct meta_block
     char data[1];
 };
 
+/*
+ * The find_suitable_block function traverses through the linked list 
+ * and find the first block which has space atleast equal to the requested space.
+ * It also sets the variable last to the address of the last block in the linked-list. 
+ * This comes handy in the malloc function
+ */
 meta_ptr find_suitable_block(meta_ptr *last, size_t size){
     meta_ptr b = base;
     while(b && !(b->free && b->size >= size)){
@@ -27,6 +52,10 @@ meta_ptr find_suitable_block(meta_ptr *last, size_t size){
     return last;
 }
 
+/*
+ * The split_space function splits the given block if it contains space greater than the requested space.
+ * Creates a new block of the free space and adds it in the linked list  
+ */
 void split_space(meta_ptr block, size_t size){
     meta_ptr new_block;
     new_block = block->data + size;
@@ -37,6 +66,11 @@ void split_space(meta_ptr block, size_t size){
     block->size = size;
 }
 
+/*
+ * extend_heap() is invoked when the already available blocks of memory is not sutiable or if no block exist already.
+ * This creates a block of memory near the break of heap.
+ * The meta_block of the newly created memory block will be added in the last of the linked list.
+ */
 meta_ptr extend_heap(meta_ptr last, size_t size){
     meta_ptr old_break, new_break;
     old_break = sbrk(0);
@@ -53,6 +87,13 @@ meta_ptr extend_heap(meta_ptr last, size_t size){
     return (old_break);
 }
 
+/*
+ * malloc is the function which will be invoked by the user to allocate space.
+ * The function first checks if there a memory block with space atleast equal to the requested space.
+ * If not then it requests a new block to be created from the heap.
+ * If there are no elements in the linked-list then it asks the heap to allocate memory block
+ * Finally it return the address from where the data can be stored.
+ */
 void* malloc(size_t size){
     meta_ptr block,last;
     size_t s;
