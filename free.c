ce78362c0e5d34564c49809fd0d85b39294ae312
Ragul1902 <ragulsenthilkumar02@gmail.com>
2021-11-20 02:31:38 +0530

    Add comments

--- free.c
+++ free.c
@@ -3,6 +3,14 @@
 #include<stdio.h>
 #include"malloc.c"
 
+
+/*
+ * This file contains the implementaion of the function free()
+ * For the deatails on few data structs such as meta_block and meta_ptr refer to the file malloc.c
+ */
+
+
+/* The function merge_block() checks if the next block is free and merges it with the block passed as an argument */
 meta_ptr merge_blocks(meta_ptr block){
     if(block->next && block->next->free){
         block->size += META_BLOCK_SIZE + block->next->size;
@@ -14,6 +22,9 @@ meta_ptr merge_blocks(meta_ptr block){
     return block;
 }
 
+/* The function get_block_addr() returns the address of the meta_block from taking the addr of data as an argument.
+ * Type casing the passed void pointer to char pointer and subtracting the size of the meta_block will give us the required address. 
+ */
 meta_ptr get_block_addr(void *p){
     char *tmp = p;
     tmp = tmp - META_BLOCK_SIZE;
@@ -21,6 +32,10 @@ meta_ptr get_block_addr(void *p){
     return (p);
 }
 
+/*
+ * This function checks if the given pointer address is indeed created by invoking malloc() or not.
+ * We use the field ptr in the meta_block() to check if the passed address is same as the one present in the meta_block()
+ */
 int is_addr_valid(void *p){
     if(base){
         if(p > base && p < sbrk(0)){
@@ -30,6 +45,13 @@ int is_addr_valid(void *p){
     return 0;
 }
 
+/*
+ * The pointer is first checked if valid. If it's valid we set the free field value of the block to 1
+ * Then if the previous block exists, it is checked if its free and then merged with the current block.
+ * Same is done for the next block also.
+ * And finally if the freed block is at the end of the linkedlist, it is removed from the linkedlist and the break line
+ * of the heap is set to the corresponding last address in the linkedlist using the syscall brk()  
+ */
 void free(void *ptr){
     if(is_addr_valid(ptr)){
         meta_ptr block = get_block_addr(ptr);
